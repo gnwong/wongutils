cdf204c1e66e59f88867363c441181f25c962653
George N. Wong <gnwong@ias.edu>
2025-06-10 15:16:30 -0400

    add simple visit session loader

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="wongutils",
-      version="0.1.6",
+      version="0.1.7",
       description="",
       url="https://github.com/gnwong/wongutils",
       author="gnwong",
